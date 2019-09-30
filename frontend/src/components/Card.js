b393605dd5347730b3f029d27cd37d52c3b06b85
YauheniRusinchuk <ruevgal@gmail.com>
2019-10-01 02:50:25 +0300

    add btn new card

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,14 +5,15 @@ function Card() {
     return (
         <div className='cart_container'>
             <p>КАРТЫ</p>
-
             <div className='carts_lists'>
+                <button className='new_add_cart'>
+                    ДОБАВИТЬ НОВУЮ КАРТУ
+                </button>
                 <CardView />
                 <CardView />
                 <CardView />
                 <CardView />
                 <CardView />
-                
             </div>
         </div>
     )
