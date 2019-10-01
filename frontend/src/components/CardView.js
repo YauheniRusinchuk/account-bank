dae94f69ff5e623fd0885500b233e935c0579227
YauheniRusinchuk <ruevgal@gmail.com>
2019-10-01 14:47:50 +0300

    add icon delete for card

--- frontend/src/components/CardView.js
+++ frontend/src/components/CardView.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import icon_del from '../assets/delete.png'
+
 
 
 function CardView() {
     return (
         <div className='cartview_container'>
-            <span>4222-2321-2342-2314</span>
+
+            <img className='delete_btn_card' src={icon_del} alt='card_delete' />
+
+            <Link to='/'>4222-2321-2342-2314</Link>
             <p>01/20</p>
             <p>Eugene Rusinchuk</p>
         </div>
