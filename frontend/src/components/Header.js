9578f280f88bb5495395f9f2d6fae686d1cabd99
YauheniRusinchuk <ruevgal@gmail.com>
2019-10-01 19:27:12 +0300

    change main page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     return (
         <div className='header_container'>
             <div className='header_menu'>
-                <Link to="/">Последние действия</Link>
+                <Link to="/">Главная</Link>
                 <Link to="/cards">Мои карты</Link>
                 <Link to='/todo'>Мои заметки</Link>
                 <Link to='/'>Мои платежи</Link>
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,28 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import cards_photo from '../assets/debit-card.png'
+import notes from '../assets/notes.png'
+import cash from '../assets/cash.png'
 
 function Main() {
     return (
         <div className='main_container'>
-            <p>ПОСЛЕДНИЕ ДЕЙСТВИЯ</p>
+            <p>ГЛАВНАЯ</p>
             <div className='main_wrapper_container'>
-                
+                <div className='list_menu'>
+                    <Link to='/cards'>
+                        <img src={cards_photo} alt='cards_img' />
+                        <p>Мои карты</p>
+                    </Link>
+                    <Link to='/cards'>
+                        <img src={notes} alt='cards_img' />
+                        <p>Мои заметки</p>
+                    </Link>
+                    <Link to='/cards'>
+                        <img src={cash} alt='cards_img' />
+                        <p>Мои платежи</p>
+                    </Link>
+                </div>
             </div>
         </div>
     )
